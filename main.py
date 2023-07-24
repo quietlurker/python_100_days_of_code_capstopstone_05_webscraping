9103c7ae7b5eee64bbe840998b2e8374ca052076
quiet_lurker <ghosia.spam@gmail.com>
2023-07-24 18:19:54 +0200

    part2-selenium with google forms

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from beautiful_soup_stuff import ZillowOffers
-# google form link:
-google_form = "https://docs.google.com/forms/d/e/1FAIpQLSc3d7bBiq3xuWwLQGaiok9pY5dB85VDtm5kvtYQ5kauIe_kFg/viewform?usp=sf_link"
+from google_forms_stuff import GoogleForm
 
-offers = ZillowOffers()
+zillow_url = "https://www.zillow.com/san-francisco-ca/rentals/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22north%22%3A37.88646946005285%2C%22east%22%3A-122.23248568896484%2C%22south%22%3A37.66394700757486%2C%22west%22%3A-122.63417331103516%7D%2C%22mapZoom%22%3A12%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A20330%2C%22regionType%22%3A6%7D%5D%7D"
 
+# get offers from zillow
+offers = ZillowOffers(zillow_url)
 offers.get_listings()
-print(offers.addresses)
-print(offers.links)
-print(offers.prices)
 
+# create answers to google form - tuple of (addresses, prices, links)
+listings = [(offers.addresses[i], offers.prices[i], offers.links[i]) for i in range(len(offers.addresses))]
 
-
-
+# send all answers to google form
+form = GoogleForm()
+form.fill_form(listings)
 
